8aee0bce00358962e336da58c1d580bbeffc74b0
Reid Netterville III <reidiiius@gmail.com>
2016-02-28 17:45:56 -0600

    160228.0

--- scripts/saucer.js
+++ scripts/saucer.js
@@ -3,24 +3,39 @@
 (function() {
 
 var teaPotHead = document.getElementsByTagName("head")[0];
-var xArt = document.getElementById("art");
-  xArt.insertAdjacentHTML("afterbegin", teaPotHead.dataset.head);
 
 var teaPotTitle = document.getElementsByTagName("title")[0];
-var xHdr = document.getElementById("hdr");
-  xHdr.textContent = teaPotTitle.dataset.title;
 
-var xTxt = document.createTextNode(xHdr.textContent);
 var xBdy = document.getElementsByTagName("body")[0];
-  xBdy.appendChild(xTxt);
+
+var xHdr = document.getElementById("hdr");
+
+var xArt = document.getElementById("art");
+
+var xSpn = document.createElement("span");
+
+var xTxt = document.createTextNode("\u2600");
 
 var teapoTeche = "url(\'images\/teapoteche.svg\')";
 
 var artEcho = document.querySelector("article");
+
+requestAnimationFrame(function() {
+  xHdr.textContent = teaPotTitle.dataset.title;
+  xArt.insertAdjacentHTML("afterbegin", teaPotHead.dataset.head);
+
   artEcho.style.backgroundImage    = teapoTeche;
   artEcho.style.backgroundColor    = "Beige";
   artEcho.style.backgroundPosition = "80% 35%";
   artEcho.style.backgroundRepeat   = "no-repeat";
+
+  xSpn.style.marginLeft = "20%";
+  xSpn.style.fontSize = "64px";
+  xSpn.style.color = "Gold";
+  xSpn.style.textShadow = "0.85em 0.33em 0.05em SkyBlue"
+  xSpn.appendChild(xTxt);
+  xBdy.appendChild(xSpn);
+});
 
 })();
 
--- scripts/teapot.js
+++ scripts/teapot.js
@@ -17,6 +17,8 @@ var Page = {
   Page.title   = document.createElement("title");
   Page.title  .textContent = btoa("\x49\x27\x6D\x20\x61\x20\x54\x65\x61\x70\x6F\x74");
   Page.title  .setAttribute("data-title", Page.title.textContent);
+
+  Page.head   .setAttribute("data-head", Page.title.textContent);
   Page.head   .appendChild(Page.title);
 
   Page["link favicon"]    = document.createElement("link");
@@ -47,79 +49,22 @@ var Page = {
     for (var i = 0; i < str.length; i++){
       arsci.push(str.charCodeAt(i));
     }
-    return arsci.join("");
+    return "A" + arsci.join("");
   };
 
-  Page.head   .setAttribute("data-head", Page.title.textContent);
   Page.body   .setAttribute("id", Page.codex(Page.token));
-
-  Page.canvas  = document.createElement("canvas");
-  Page.canvas .setAttribute("id", "cnv");
-  Page.canvas .setAttribute("width", innerWidth);
-  Page.canvas .setAttribute("height", innerHeight);
-  Page.body   .appendChild(Page.canvas);
-
-  Page.header  = document.createElement("header");
-  Page.header .setAttribute("id", "hdr");
-  Page.body   .appendChild(Page.header);
-
-  Page.article = document.createElement("article");
-  Page.article.setAttribute("id", "art");
-  Page.body   .appendChild(Page.article);
-
-  Page.section = document.createElement("section");
-  Page.section.setAttribute("id", "sec");
-  Page.section.textContent = Page.title.textContent;
-  Page.article.appendChild(Page.section);
-
-  Page.aside   = document.createElement("aside");
-  Page.aside  .setAttribute("id", "asd");
-  Page.aside  .setAttribute("data-aside", Page.title.textContent);
-  Page.aside  .textContent = Page.section.textContent.toLowerCase();
-  Page.body   .insertBefore(Page.aside, Page.article.nextSibling);
-
-  Page.footer  = document.createElement("footer");
-  Page.footer .setAttribute("id", "ftr");
-  Page.footer .textContent = Page.aside.textContent;
-  Page.body   .appendChild(Page.footer);
-
-  Page["image teapoteche"] = document.createElement("img");
-  Page["image teapoteche"].setAttribute("src", "images\/teapoteche.svg");
-  Page["image teapoteche"].setAttribute("alt", Page.title.textContent);
-  Page["image teapoteche"].setAttribute("width", "auto");
-  Page["image teapoteche"].setAttribute("height", "auto");
-  Page.article.insertBefore(Page["image teapoteche"], Page.section);
-
-  Page.br      = document.createElement("br");
-  Page.article.insertBefore(Page.br, Page["image teapoteche"]);
-
-  Page.div     = document.createElement("div");
-  Page.div    .setAttribute("id", "dv");
-  Page.div    .appendChild(document.createTextNode(Page.title.textContent));
-  Page.article.insertBefore(Page.div,   Page.section.nextSibling);
-
-  Page.anchor  = document.createElement("a");
-  Page.anchor .setAttribute("name", Page.title.textContent);
-  Page.body   .insertBefore(Page.anchor, Page.article);
-
-  Page.pre     = document.createElement("pre");
-  Page.pre    .setAttribute("id", "pr");
-  Page.section.appendChild(Page.pre);
-
-  Page.code    = document.createElement("code");
-  Page.code   .setAttribute("data-code", Page.title.textContent);
-  Page.pre    .appendChild(Page.code);
-
-  Page.span    = document.createElement("span");
-  Page.span   .setAttribute("style", "display:none");
-  Page.span   .textContent = Page.title.textContent;
-  Page.code   .appendChild(Page.span);
-
   Page.body    .style.margin = 0;
   Page.body.style.paddingTop = (innerHeight/1122.5).toFixed(3) + "em";
   Page.body.style.fontFamily = "\'DejaVu Sans Mono\', monospace";
   Page.body  .style.fontSize = "32px";
   Page.body     .style.color = "DarkSlateGrey";
+
+  Page.ostracon = document.createDocumentFragment();
+
+  Page.canvas  = document.createElement("canvas");
+  Page.canvas .setAttribute("id", "cnv");
+  Page.canvas .setAttribute("width", innerWidth);
+  Page.canvas .setAttribute("height", innerHeight);
 
   Page.canvas .style.position = "fixed";
   Page.canvas .style.zIndex   = "-1";
@@ -140,27 +85,85 @@ CanvasContext.lineTo((innerWidth/1.498).toFixed(3), 0);
    CanvasContext.strokeStyle = "Wheat";
     CanvasContext.stroke();
 
+  Page.ostracon.appendChild(Page.canvas);
+
+  Page.header  = document.createElement("header");
+  Page.header .setAttribute("id", "hdr");
   Page.header .style.opacity = 0.4;
-  Page.article.style.opacity = 1.0;
-  Page["image teapoteche"].style.opacity = 0.9;
-  Page.section.style.opacity = 0.1;
-  Page.div    .style.opacity = 0.3;
-  Page.aside  .style.opacity = 0.5;
-  Page.footer .style.opacity = 0.7;
-
   Page.header .style.paddingLeft = (innerWidth/672.7).toFixed(3) + "em";
-  Page.article.style.paddingLeft = (innerWidth/336.4).toFixed(3) + "em";
-  Page["image teapoteche"].style.paddingLeft = (innerWidth/224.5).toFixed(3) + "em";
-  Page.section.style.paddingLeft = (innerWidth/336.4).toFixed(3) + "em";
-  Page.div    .style.paddingLeft = (innerWidth/672.7).toFixed(3) + "em";
-  Page.aside  .style.paddingLeft = (innerWidth/336.4).toFixed(3) + "em";
-  Page.footer .style.paddingLeft = (innerWidth/672.7).toFixed(3) + "em";
 
-  for (var i = 0; i < Page.body.children.length; i++) {
-    Page.body.children[i + 1].style.paddingTop = (innerHeight/1259.9).toFixed(3) + "em";
-    Page.body.children[i + 1].style.width      = "85%";
-    Page.body.children[i + 1].style.border     = "1px dotted LightSkyBlue";
-  }
+  Page.ostracon.appendChild(Page.header);
+
+  Page.article = document.createElement("article");
+  Page.article.setAttribute("id", "art");
+  Page.article.style.width = "61.8%";
+  Page.article.style.opacity = 1.0;
+  Page.article.style.paddingLeft = (innerWidth/336.4).toFixed(3) + "em";
+
+  Page.section = document.createElement("section");
+  Page.section.setAttribute("id", "sec");
+  Page.section.style.opacity = 0.1;
+  Page.section.style.paddingLeft = (innerWidth/336.4).toFixed(3) + "em";
+  Page.section.textContent = Page.title.textContent;
+  Page.article.appendChild(Page.section);
+
+  Page.pre     = document.createElement("pre");
+  Page.pre    .setAttribute("id", "pr");
+  Page.section.appendChild(Page.pre);
+
+  Page.code    = document.createElement("code");
+  Page.code   .setAttribute("data-code", Page.title.textContent);
+  Page.pre    .appendChild(Page.code);
+
+  Page.span    = document.createElement("span");
+  Page.span   .setAttribute("style", "display:none");
+  Page.span   .textContent = Page.title.textContent;
+  Page.code   .appendChild(Page.span);
+
+  Page["image teapoteche"] = document.createElement("img");
+  Page["image teapoteche"].setAttribute("src", "images\/teapoteche.svg");
+  Page["image teapoteche"].setAttribute("alt", Page.title.textContent);
+  Page["image teapoteche"].setAttribute("width", "auto");
+  Page["image teapoteche"].setAttribute("height", "auto");
+  Page["image teapoteche"].style.opacity = 0.9;
+  Page["image teapoteche"].style.paddingLeft = (innerWidth/224.5).toFixed(3) + "em";
+  Page.article.insertBefore(Page["image teapoteche"], Page.section);
+
+  Page.br      = document.createElement("br");
+  Page.article.insertBefore(Page.br, Page["image teapoteche"]);
+
+  Page.div     = document.createElement("div");
+  Page.div    .setAttribute("id", "dv");
+  Page.div    .style.opacity = 0.3;
+  Page.div    .style.paddingLeft = (innerWidth/672.7).toFixed(3) + "em";
+  Page.div    .appendChild(document.createTextNode(Page.title.textContent));
+  Page.article.insertBefore(Page.div, Page.section.nextSibling);
+
+  Page.ostracon.appendChild(Page.article);
+
+  Page.aside   = document.createElement("aside");
+  Page.aside  .setAttribute("id", "asd");
+  Page.aside  .setAttribute("data-aside", Page.title.textContent);
+  Page.aside  .style.opacity = 0.5;
+  Page.aside  .style.paddingLeft = (innerWidth/336.4).toFixed(3) + "em";
+  Page.aside  .textContent = Page.section.textContent.toLowerCase();
+
+  Page.ostracon.insertBefore(Page.aside, Page.article.nextSibling);
+
+  Page.anchor  = document.createElement("a");
+  Page.anchor .setAttribute("name", Page.title.textContent);
+
+  Page.ostracon.insertBefore(Page.anchor, Page.article);
+
+  Page.footer  = document.createElement("footer");
+  Page.footer .setAttribute("id", "ftr");
+  Page.footer .style.opacity = 0.7;
+  Page.footer .style.paddingLeft = (innerWidth/672.7).toFixed(3) + "em";
+  Page.footer .textContent = Page.aside.textContent;
+
+  Page.ostracon.appendChild(Page.footer);
+
+  Page.body.appendChild(Page.ostracon);
 
 };
 
